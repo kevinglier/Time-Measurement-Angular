7ddf949b1a8556ff7536f8b7e44c9991d02becbb
Kevin Glier <k.glier@objement.com>
2018-04-13 09:12:34 +0200

    Implement settings component

--- src/app/time-measurement/settings/settings.component.ts
+++ src/app/time-measurement/settings/settings.component.ts
@@ -1,64 +1,87 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
-import { TimerType, TimerUnit, TimeMeasurementSettings } from '../settings';
+import {Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
+import {TimerType, TimerUnit, TimeMeasurementSettings} from '../settings';
 
 @Component({
-  selector: 'settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+    selector: 'settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit, OnChanges {
 
-  timerTypeOptions = [
-    { caption: 'Timer', value: TimerType.timer },
-    { caption: 'Counter', value: TimerType.counter },
-  ]
-  timerUnitOptions = [
-    { caption: 'Pomodoro', value: TimerUnit.pomodoro },
-    { caption: 'Hour', value: TimerUnit.hour },
-    { caption: 'Custom', value: TimerUnit.custom },
-  ]
-  timerCustomValueMinutesValues = (() => { var x = []; for (var i = 0; i <= 120; i+=5) x.push(i); return x; })();
-  timerCustomValueSecondsValues = (() => { var x = []; for (var i = 0; i <= 45; i+=15) x.push(i); return x; })();
+    timerTypeOptions = [
+        {caption: 'Timer', value: TimerType.timer},
+        {caption: 'Counter', value: TimerType.counter},
+    ]
+    timerUnitOptions = [
+        {caption: 'Pomodoro', value: TimerUnit.pomodoro},
+        {caption: 'Hour', value: TimerUnit.hour},
+        {caption: 'Custom', value: TimerUnit.custom},
+    ]
+    timerCustomValueMinutesValues = (() => {
+        var x = [];
+        for (var i = 0; i <= 120; i += 5) x.push(i);
+        return x;
+    })();
+    timerCustomValueSecondsValues = (() => {
+        var x = [];
+        for (var i = 0; i <= 45; i += 15) x.push(i);
+        return x;
+    })();
 
 
-  @Input() settings;
-  @Output() settingsChange = new EventEmitter<TimeMeasurementSettings>();
+    @Input() settings;
+    @Output() settingsChange = new EventEmitter<TimeMeasurementSettings>();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['settings']) {
-      console.log('changes', changes);
-      var m = this.settings.timer.customValue ? parseInt((this.settings.timer.customValue / 60) + '') : 15;
-      var s = this.settings.timer.customValue ? this.settings.timer.customValue % 60 : 0;
-      console.log('m', m);
-      console.log('s', s);
-      this.timerCustomValueMinutes = m;
-      this.timerCustomValueSeconds = s;
+    constructor() {
     }
-  }
 
-  timerTypeChange(type) {
-    this.settings.timer.type = type;
-    this.settingsChange.emit(this.settings);
-  }
+    ngOnInit() {
+    }
 
-  timerUnitChange(type) {
-    this.settings.timer.unit = type;
-    this.settingsChange.emit(this.settings);
-  }
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['settings']) {
+            console.log('changes', changes);
+            var m = this.settings.timer.customValue ? parseInt((this.settings.timer.customValue / 60) + '') : 15;
+            var s = this.settings.timer.customValue ? this.settings.timer.customValue % 60 : 0;
+            console.log('m', m);
+            console.log('s', s);
+            this.timerCustomValueMinutes = m;
+            this.timerCustomValueSeconds = s;
+        }
+    }
 
-  timerCustomValueMinutes = 15;
-  timerCustomValueMinutesChange(minutes) {
-    this.settings.timer.customValue = (this.timerCustomValueMinutes * 60) + this.timerCustomValueSeconds;
-    this.settingsChange.emit(this.settings);
-  }
-  timerCustomValueSeconds = 0;
-  timerCustomValueSecondsChange(seconds) {
-    this.settings.timer.customValue = (this.timerCustomValueMinutes * 60) + this.timerCustomValueSeconds;
-    this.settingsChange.emit(this.settings);
-  }
+    timerTypeChange(type) {
+        this.settings.timer.type = type;
+        this.settingsChange.emit(this.settings);
+    }
+
+    timerUnitChange(type) {
+        this.settings.timer.unit = type;
+        this.settingsChange.emit(this.settings);
+    }
+
+    private _timerCustomValueMinutes = 15;
+    get timerCustomValueMinutes() {
+        return this._timerCustomValueMinutes;
+    }
+
+    set timerCustomValueMinutes(minutes) {
+        console.log('MIN', minutes, this.timerCustomValueMinutes);
+        this._timerCustomValueMinutes = minutes;
+        this.settings.timer.customValue = (this.timerCustomValueMinutes * 60) + this.timerCustomValueSeconds;
+        this.settingsChange.emit(this.settings);
+    }
+
+
+    private _timerCustomValueSeconds = 0;
+    get timerCustomValueSeconds() {
+        return this._timerCustomValueSeconds;
+    }
+
+    set timerCustomValueSeconds(seconds) {
+        this._timerCustomValueSeconds = seconds;
+        this.settings.timer.customValue = (this.timerCustomValueMinutes * 60) + this.timerCustomValueSeconds;
+        this.settingsChange.emit(this.settings);
+    }
+
 }
--- src/app/time-measurement/time-measurement.component.ts
+++ src/app/time-measurement/time-measurement.component.ts
@@ -1,47 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { TimeMeasurementService } from './time-measurement.service';
-import { TimeMeasurementSettings, TimerType, TimerUnit } from './settings';
+import {Component, OnInit} from '@angular/core';
+import {TimeMeasurementService} from './time-measurement.service';
+import {TimeMeasurementSettings, TimerType, TimerUnit} from './settings';
 
 @Component({
-  selector: 'time-measurement',
-  templateUrl: './time-measurement.component.html',
-  styleUrls: ['./time-measurement.component.css']
+    selector: 'time-measurement',
+    templateUrl: './time-measurement.component.html',
+    styleUrls: ['./time-measurement.component.css']
 })
 export class TimeMeasurementComponent implements OnInit {
-  
-  public timeTableEntries = this.timeMeasurementService.timeTableEntries;
 
-  public settings = <TimeMeasurementSettings>{
-    timer: {
-      type: TimerType.timer,
-      unit: TimerUnit.pomodoro
+    public timeTableEntries = this.timeMeasurementService.timeTableEntries;
+
+    public settings = <TimeMeasurementSettings>{
+        timer: {
+            type: TimerType.timer,
+            unit: TimerUnit.pomodoro,
+            customValue: 15 * 60
+        }
+    };
+
+    public get timerStartValue() {
+        console.log(this.settings);
+        if (this.settings.timer.unit != TimerUnit.custom) {
+            switch (+this.settings.timer.unit) {
+                case TimerUnit.pomodoro:
+                    return 25 * 60;
+                case TimerUnit.hour:
+                    return 60 * 60;
+            }
+        }
+
+        return this.settings.timer.customValue || 15 * 60;
     }
-  };
-  
-  constructor(
-    private timeMeasurementService: TimeMeasurementService
-  ) { }
 
-  ngOnInit() {
-  }
-  
-  timerTick(event) {
-    
-  }
-  
-  timerStarted(event) {
-    
-  }
-  
-  timerStopped(event) {
-    
-  }
-  
-  timerPaused(event) {
-    
-  }
-  
-  settingsChanged(event) {
-    
-  }
+    constructor(
+        private timeMeasurementService: TimeMeasurementService
+    ) {
+    }
+
+    ngOnInit() {
+    }
+
+    timerTick(event) {
+
+    }
+
+    timerStarted(event) {
+
+    }
+
+    timerStopped(event) {
+
+    }
+
+    timerPaused(event) {
+
+    }
+
+    settingsChanged(event) {
+
+    }
 }
