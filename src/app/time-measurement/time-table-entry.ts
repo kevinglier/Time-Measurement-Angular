3a369b1cf67424bfb96c986b9d9b1538b68949c0
Kevin Glier <glier@arcor.de>
2018-04-24 00:02:23 +0200

    Add property "isSystemEntry"

--- src/app/time-measurement/time-table-entry.ts
+++ src/app/time-measurement/time-table-entry.ts
@@ -5,4 +5,6 @@ export class TimeTableEntry {
     time: Date;
     /** The duration (in seconds) the action took. */
     duration: number;
+    /** It is an entry managed by the system with should be handled as a special item (e.g. no rights to edit time/duration) */
+    isSystemEntry: boolean;
 }
