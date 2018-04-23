d38af5f0053b15a0e4515437acf91a1ba8a5dcf1
Kevin Glier <glier@arcor.de>
2018-04-24 00:03:52 +0200

    Add the ability to store and read the time table using LocalStorage

--- src/app/time-measurement/tm-time-measurement.service.ts
+++ src/app/time-measurement/tm-time-measurement.service.ts
@@ -4,50 +4,86 @@ import {TimeTableEntry} from './time-table-entry';
 import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
+const LOCALSTORAGE_ENTRIES_KEY = '_timeTableEntries';
 
 @Injectable()
 export class TmTimeMeasurementService {
 
-    private _timeTableEntries = [];
-    private timeTableEntriesSubject: BehaviorSubject<TimeTableEntry[]> = new BehaviorSubject<TimeTableEntry[]>([]);
-    public timeTableEntries$: Observable<TimeTableEntry[]> = this.timeTableEntriesSubject.asObservable();
+  private _timeTableEntries: TimeTableEntry[] = [];
+  private timeTableEntriesSubject: BehaviorSubject<TimeTableEntry[]> = new BehaviorSubject<TimeTableEntry[]>([]);
+  public timeTableEntries$: Observable<TimeTableEntry[]> = this.timeTableEntriesSubject.asObservable();
 
-    public get timeTableEntries() {
-        return this.timeTableEntriesSubject.getValue();
+  public get timeTableEntries() {
+    return this.timeTableEntriesSubject.getValue();
+  }
+
+  constructor() {
+    this.readFromStorage();
+
+    this.timeTableEntries$.subscribe(entries => {
+      this.saveToStorage();
+    });
+  }
+
+  addTimeTableEntry(entry: TimeTableEntry) {
+    this._timeTableEntries.push(entry);
+    this.timeTableEntriesSubject.next(this._timeTableEntries);
+
+    return entry;
+  }
+
+  removeTimeTableEntry(entry: TimeTableEntry) {
+    const index = this._timeTableEntries.indexOf(entry);
+    if (index > -1) {
+      this._timeTableEntries.splice(index, 1);
+      this.timeTableEntriesSubject.next(this._timeTableEntries);
+
+      return true;
     }
 
-    constructor() {
+    return false;
+  }
+
+  editTimeTableEntry(oldEntry: TimeTableEntry, newEntry: TimeTableEntry) {
+    const index = this._timeTableEntries.indexOf(oldEntry);
+    if (index >= -1) {
+      this._timeTableEntries[index] = newEntry;
+      this.timeTableEntriesSubject.next(this._timeTableEntries);
+
+      return newEntry;
     }
 
-    addTimeTableEntry(entry: TimeTableEntry) {
-        this._timeTableEntries.push(entry);
+    return null;
+  }
+
+  private saveToStorage() {
+
+    if (this._timeTableEntries && (this._timeTableEntries.length > 0 || localStorage.getItem(LOCALSTORAGE_ENTRIES_KEY))) {
+      localStorage.setItem(LOCALSTORAGE_ENTRIES_KEY, JSON.stringify(this._timeTableEntries));
+    }
+  }
+
+  private readFromStorage() {
+
+    const jsonString = localStorage.getItem(LOCALSTORAGE_ENTRIES_KEY);
+    if (jsonString) {
+
+      const entries = JSON.parse(jsonString, (key, value) => {
+        switch (key) {
+          case 'time':
+            return new Date(value);
+          case 'duration':
+            return +value;
+        }
+        return value;
+      });
+
+      if (entries && entries.length > 0) {
+        console.log(entries);
+        this._timeTableEntries = entries;
         this.timeTableEntriesSubject.next(this._timeTableEntries);
-
-        return entry;
+      }
     }
-
-    removeTimeTableEntry(entry: TimeTableEntry) {
-        const index = this._timeTableEntries.indexOf(entry);
-        if (index > 0) {
-            this._timeTableEntries = this._timeTableEntries.slice(index, 1);
-            this.timeTableEntriesSubject.next(this._timeTableEntries);
-
-            return true;
-        }
-
-        return false;
-    }
-
-    editTimeTableEntry(oldEntry: TimeTableEntry, newEntry: TimeTableEntry) {
-        const index = this._timeTableEntries.indexOf(oldEntry);
-        if (index >= 0) {
-            this._timeTableEntries[index] = newEntry;
-            this.timeTableEntriesSubject.next(this._timeTableEntries);
-
-            return newEntry;
-        }
-
-        return null;
-    }
+  }
 
 }
